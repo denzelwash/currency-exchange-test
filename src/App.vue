57459fb28bde9b170bbb5dbfa9f4fc69ce3156b3
Denis Obolevich <denzelwash33@yandex.ru>
2025-04-11 17:17:32 +0300

    Перенес получение валют в App

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,17 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import PageHeader from './components/PageHeader.vue'
+
+import { getCurrency } from '@/services/queries'
+import { onBeforeMount } from 'vue'
+import { useCurrencyStore } from './stores/currency'
+
+const currencyStore = useCurrencyStore()
+
+onBeforeMount(async () => {
+  const currencyRate = await getCurrency()
+  if (currencyRate) {
+    currencyStore.currencyRate = currencyRate
+  }
+})
 </script>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -2,12 +2,4 @@
   <div>Главная</div>
 </template>
 
-<script setup lang="ts">
-import { getCurrency } from '@/services/queries'
-import { onBeforeMount } from 'vue'
-
-onBeforeMount(async () => {
-  const currency = await getCurrency()
-  console.log(currency)
-})
-</script>
+<script setup lang="ts"></script>
