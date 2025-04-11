65570ca279de9c7266c8ef07588f04d83ba38df5
Denis Obolevich <denzelwash33@yandex.ru>
2025-04-11 17:37:29 +0300

    Вывод на главной

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>Главная</div>
+  <div v-if="currencyStore.currencyRate">
+    <ul class="list">
+      <li v-for="currency in list" :key="currency">
+        {{ `1 ${currency} = ${calc(currency)} ${currencyStore.currencyActive}` }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { availableСurrencies } from '@/const'
+import { useCurrencyStore } from '@/stores/currency'
+import { formatNumber } from '@/utils/formatNumber'
+import { computed } from 'vue'
+
+const currencyStore = useCurrencyStore()
+
+const list = computed(() => {
+  return availableСurrencies.filter((c) => c !== currencyStore.currencyActive)
+})
+
+const calc = (currency: string) => {
+  return formatNumber(currencyStore.currencyRate![`${currency}-${currencyStore.currencyActive}`])
+}
+</script>
+
+<style scoped lang="scss">
+.list {
+  display: grid;
+  gap: 8px;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  li {
+    text-transform: uppercase;
+  }
+}
+</style>
