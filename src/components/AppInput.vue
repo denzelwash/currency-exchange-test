2966f3576a925de9d492fa5c2a1764dacecbe735
Denis Obolevich <denzelwash33@yandex.ru>
2025-04-12 16:28:03 +0300

    Сделал компонент инпута числовым

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,13 +1,9 @@
 <template>
-  <input
-    :class="['input', props.class]"
-    type="text"
-    :value="props.modelValue"
-    @input="(value) => emit('update:modelValue', value)"
-  />
+  <input :class="['input', props.class]" :value="props.modelValue" @input="inputHandler" />
 </template>
 
 <script setup lang="ts">
+import { getCurrentInstance } from 'vue'
 interface Props {
   modelValue: string
   type?: 'text' | 'number'
@@ -17,8 +13,24 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   type: 'text',
 })
-
 const emit = defineEmits(['update:modelValue'])
+const instance = getCurrentInstance()
+
+const inputHandler = (e: Event) => {
+  const value = (e.target as HTMLInputElement).value
+  if (props.type === 'text') {
+    emit('update:modelValue', value)
+  }
+  if (props.type === 'number') {
+    const newValue = value
+      .replace(/[^\d.]/g, '')
+      .replace(/^\./, '')
+      .replace(/(\..*)\./g, '$1')
+      .replace(/(\.\d{2})\d+/, '$1')
+    emit('update:modelValue', newValue)
+    instance!.proxy!.$forceUpdate()
+  }
+}
 </script>
 
 <style lang="scss" scoped>
