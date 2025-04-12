bdc9330bdf7d60d5b2adcf2e0226d78a3409f49b
Denis Obolevich <denzelwash33@yandex.ru>
2025-04-12 18:20:16 +0300

    Фикс роутера

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import ConvertView from '@/views/ConvertView.vue'
 import IndexView from '@/views/IndexView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
